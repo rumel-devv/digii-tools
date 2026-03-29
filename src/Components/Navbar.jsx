238700c47eda0a72f23c0c9808744b721bd78420
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-03-29 21:54:17 +0600

    complted navbar design

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,24 @@
+import cartImg from '../assets/products/shopping-cart.png'
+
 const NavBar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar w-full md:w-10/12 mx-auto ">
       <div className="navbar-start">
-        <div className="flex items-center gap-1 font-bold text-xl">
-          <img className="w-10" src="/logo.png" /> Ai Hub
-        </div>
+          <h2 className="flex text-[#6C29F8] items-center gap-1 font-bold text-2xl">DigiTools</h2> 
       </div>
       <div className="navbar-center hidden md:flex">
-        <ul className="menu menu-horizontal gap-10 px-1 text-lg">
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>About</a>
-          </li>
-          <li>
-            <a>Services</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
+        <ul className="menu menu-horizontal gap-5 px-1 text-[16px]">
+          <li><a>Product</a></li>
+          <li> <a>Features</a> </li>
+          <li> <a>Pricing</a></li>
+          <li><a>Testimonial</a> </li>
+          <li> <a>FAQ</a> </li>
         </ul>
       </div>
-      <div className="navbar-end gap-5">
-        <a className="btn bg-red-500 rounded-full text-white">Get in Touch</a>
+      <div className="navbar-end gap-3">
+        <img src={cartImg} alt='Cart Image'></img>
+        <p className="hidden md:block">Login</p>
+        <a className="btn bg-[#6C29F8] rounded-full text-white">Get in Touch</a>
       </div>
     </div>
   );
