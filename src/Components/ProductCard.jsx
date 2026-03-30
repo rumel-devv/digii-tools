825df797e8e527b5eb4bb8a8e9ddd4047fc3b3be
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-03-30 19:32:15 +0600

    finished card rendering

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -4,18 +4,21 @@ const ProductCard = ( {product}) => {
     return (
         <div className="card w-full md:w-85 bg-base-100 shadow-sm">
   <div className="card-body">
-    <span className="badge badge-xs badge-warning flex flex-col justify-end">Most Popular</span>
-    {/* <img src={product.icon} ></img> */}
+    <div className='flex justify-end'><span className="badge badge-xs py-2 px-3 font-semibold text-white bg-[#6C29F8] "> {product.tag} </span></div>
+    <img className='w-8 h-8 border border-gray-300 rounded-full p-1' src={product.image} ></img>
     <div className='space-y-1'>
-      <h2 className="text-3xl font-bold ">Starter</h2>
-      <p>Perfect for getting started </p>
-      <span className="text-xl">$0/mo</span>
+      <h2 className="text-xl font-semibold "> {product.name} </h2>
+      <p> {product.description} </p>
+      <span className="text-xl">${product.price}/{product.period}</span>
     </div>
     <ul className="mt-6 flex flex-col gap-2 text-xs">
-      <li>
+      {product.features.map(feature =>   <li>
         <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>High-resolution image generation</span>
-      </li>
+        <span> {feature} </span>
+      </li> )}
+
+
+     
      
    
     </ul>
